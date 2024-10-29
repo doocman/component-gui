68e047963c4aa968d366b6413cba4b78193fb59f
doocman <rvonsonsnadtz@gmail.com>
2024-10-29 14:54:56 +0100

    use https not git. for fmt

--- cmake/findFmt.cmake
+++ cmake/findFmt.cmake
@@ -1,7 +1,7 @@
 
 fetchcontent_declare(
         fmt
-        GIT_REPOSITORY git@github.com:fmtlib/fmt.git
+        GIT_REPOSITORY https://github.com/fmtlib/fmt.git
         GIT_TAG 0c9fce2ffefecfdce794e1859584e25877b7b592 # 11.0.2
 )
 
