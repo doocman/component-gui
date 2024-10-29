6f8d20426b6531267250ad764d3c46d1d1051f7d
doocman <rvonsonsnadtz@gmail.com>
2024-10-29 20:15:34 +0100

    comment type_traits and utility

--- c++-src/inc/cgui/std-backport/type_traits.hpp
+++ c++-src/inc/cgui/std-backport/type_traits.hpp
@@ -12,30 +12,10 @@
 namespace cgui::bp {
 template <typename T> using dereferenced_t = decltype(*std::declval<T &&>());
 
-template <typename T, T... tVals>
-constexpr bool is_increasing(std::integer_sequence<T, tVals...>) {
-  if constexpr (sizeof...(tVals) < 2) {
-    return true;
-  } else {
-    T constexpr val_arr[] = {tVals...};
-    return std::transform_reduce(
-        std::begin(val_arr), std::end(val_arr) - 1, std::begin(val_arr) + 1,
-        bool{}, std::logical_and{}, [](auto const &l, auto const &r) {
-          return r - l == static_cast<T>(1);
-        });
-  }
-}
-
-template <typename T> struct remove_rvalue_reference {
-  using type = T;
-};
-template <typename T>
-struct remove_rvalue_reference<T &&> : remove_rvalue_reference<T> {};
-template <typename T>
-using remove_rvalue_reference_t = typename remove_rvalue_reference<T>::type;
-
-template <typename T>
-struct ct_value_wrapper {
+/// Compile-time value wrapper. Can simplify non-type template parameter usage
+/// as it allows for automatic deduction of its type.
+/// \tparam T wrapped type
+template <typename T> struct ct_value_wrapper {
   T value_;
   consteval explicit(false) ct_value_wrapper(T v) : value_(v) {}
   constexpr T raw() const { return value_; }
@@ -43,21 +23,7 @@ struct ct_value_wrapper {
   constexpr auto operator<=>(const ct_value_wrapper &) const = default;
   constexpr bool operator==(const ct_value_wrapper &) const = default;
 };
-template <typename T>
-ct_value_wrapper(T) -> ct_value_wrapper<T>;
-
-template <typename TEnum>
-struct value_wrapper {
-  TEnum value;
-  consteval explicit(false) value_wrapper(TEnum v) : value(v) {}
-  constexpr bool operator==(value_wrapper const&) const = default;
-  constexpr auto operator<=>(value_wrapper const&) const = default;
-};
-template <typename T>
-value_wrapper(T) -> value_wrapper<std::remove_cvref_t<T>>;
-
-template <value_wrapper v, typename T>
-struct value_type_pair {};
+template <typename T> ct_value_wrapper(T) -> ct_value_wrapper<T>;
 } // namespace cgui::bp
 
 #endif // COMPONENT_GUI_TYPE_TRAITS_HPP
