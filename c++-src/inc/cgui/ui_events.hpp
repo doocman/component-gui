9c7741b4bc3e172aa52b68d920e2a55141c939fa
doocman <rvonsonsnadtz@gmail.com>
2024-11-10 11:40:13 +0100

    rename many ui events stuff to input events stuff. move events tests to separate .cpp file

--- c++-src/inc/cgui/ui_events.hpp
+++ c++-src/inc/cgui/ui_events.hpp
@@ -9,7 +9,7 @@
 
 namespace cgui {
 
-enum class ui_events {
+enum class input_events {
   system,
   mouse_move,
   mouse_button_down,
@@ -18,57 +18,57 @@ enum class ui_events {
   window_resized,
 };
 
-template <ui_events tEvt> struct ui_event_identity {
-  static constexpr ui_events value = tEvt;
+template <input_events tEvt> struct input_event_identity {
+  static constexpr input_events value = tEvt;
 };
 
-template <ui_events> struct ui_event_constraints {
+template <input_events> struct input_event_constraints {
   template <typename> static constexpr bool type_passes = true;
 };
-template <> struct ui_event_constraints<ui_events::mouse_button_down> {
+template <> struct input_event_constraints<input_events::mouse_button_down> {
   template <typename T>
   static constexpr bool type_passes = requires(T const &t) {
     { call::position(t) } -> point_coordinate;
     call::mouse_button(t);
   };
 };
-template <> struct ui_event_constraints<ui_events::mouse_button_up> {
+template <> struct input_event_constraints<input_events::mouse_button_up> {
   template <typename T>
   static constexpr bool type_passes = requires(T const &t) {
     { call::position(t) } -> point_coordinate;
     call::mouse_button(t);
   };
 };
-template <> struct ui_event_constraints<ui_events::mouse_move> {
+template <> struct input_event_constraints<input_events::mouse_move> {
   template <typename T>
   static constexpr bool type_passes = requires(T const &t) {
     { call::position(t) } -> point_coordinate;
   };
 };
-template <> struct ui_event_constraints<ui_events::window_resized> {
+template <> struct input_event_constraints<input_events::window_resized> {
   template <typename T>
   static constexpr bool type_passes = requires(T const &t) {
     { call::size_of(t) } -> point_size_wh;
   };
 };
 
-template <typename T, ui_events tEvt>
-concept ui_event_c = ui_event_constraints<tEvt>::template type_passes<T>;
+template <typename T, input_events tEvt>
+concept input_event_c = input_event_constraints<tEvt>::template type_passes<T>;
 
 /// Return type for event deduction function. Use template parameter to indicate
 /// what events a backend event *could* be, while using the return value for
 /// what it actually *is*.
 /// \tparam evts CGUI event types that an event could be.
-template <ui_events... evts> struct subset_ui_events {
+template <input_events... evts> struct subset_input_events {
   static_assert(sizeof...(evts) > 0, "You must at least specify 1 event type");
-  ui_events val;
-  constexpr explicit(false) subset_ui_events(ui_events v) noexcept : val(v) {
+  input_events val;
+  constexpr explicit(false) subset_input_events(input_events v) noexcept : val(v) {
     CGUI_ASSERT(((v == evts) || ...));
   }
-  constexpr explicit(false) operator ui_events() const noexcept { return val; }
+  constexpr explicit(false) operator input_events() const noexcept { return val; }
 
-  template <ui_events tEvt>
-  static consteval bool can_be_event(ui_event_identity<tEvt>) noexcept {
+  template <input_events tEvt>
+  static consteval bool can_be_event(input_event_identity<tEvt>) noexcept {
     return ((tEvt == evts) || ...);
   }
 };
@@ -76,36 +76,37 @@ template <ui_events... evts> struct subset_ui_events {
 /// Single event optimisation specialisation, that lacks any member field, and
 /// also does not need any input arguments.
 /// \tparam evt Event type that this particular subset is.
-template <ui_events evt> struct subset_ui_events<evt> {
-  constexpr explicit(false) subset_ui_events(ui_events v) noexcept {
+template <input_events evt> struct subset_input_events<evt> {
+  constexpr explicit(false) subset_input_events(input_events v) noexcept {
     assert(v == evt);
     unused(v);
   }
-  constexpr subset_ui_events() noexcept = default;
-  constexpr explicit(false) operator ui_events() const noexcept { return evt; }
+  constexpr subset_input_events() noexcept = default;
+  constexpr explicit(false) operator input_events() const noexcept { return evt; }
 
-  template <ui_events tEvt>
-  static consteval bool can_be_event(ui_event_identity<tEvt> = {}) noexcept {
+  template <input_events tEvt>
+  static consteval bool can_be_event(input_event_identity<tEvt> = {}) noexcept {
     return (tEvt == evt);
   }
 };
 
 template <typename T>
-concept subset_ui_event_c = std::convertible_to<T, ui_events> && requires() {
+concept subset_input_event_c = std::convertible_to<T, input_events> && requires() {
   {
-    std::remove_cvref_t<T>::can_be_event(ui_event_identity<ui_events::system>{})
+    std::remove_cvref_t<T>::can_be_event(
+        input_event_identity<input_events::system>{})
   } -> std::convertible_to<bool>;
   {
     std::remove_cvref_t<T>::can_be_event(
-        ui_event_identity<ui_events::mouse_move>{})
+        input_event_identity<input_events::mouse_move>{})
   } -> std::convertible_to<bool>;
   {
     std::remove_cvref_t<T>::can_be_event(
-        ui_event_identity<ui_events::mouse_button_up>{})
+        input_event_identity<input_events::mouse_button_up>{})
   } -> std::convertible_to<bool>;
   {
     std::remove_cvref_t<T>::can_be_event(
-        ui_event_identity<ui_events::mouse_button_down>{})
+        input_event_identity<input_events::mouse_button_down>{})
   } -> std::convertible_to<bool>;
 };
 
@@ -114,9 +115,9 @@ namespace impl {
 struct do_event_type {
   template <typename T>
     requires(requires(T &&t) {
-      { _do_event_type{}(std::forward<T>(t)) } -> subset_ui_event_c;
+      { _do_event_type{}(std::forward<T>(t)) } -> subset_input_event_c;
     })
-  constexpr subset_ui_event_c auto operator()(T &&t) const {
+  constexpr subset_input_event_c auto operator()(T &&t) const {
     return _do_event_type{}(std::forward<T>(t));
   }
 };
@@ -128,53 +129,53 @@ template <typename T>
 concept has_event_type =
     requires(bp::as_forward<T> t) { call::event_type(*t); };
 
-template <ui_events tEvt, typename T> consteval bool can_be_event() {
+template <input_events tEvt, typename T> consteval bool can_be_event() {
   if constexpr (has_event_type<T>) {
     using subset_t =
         std::remove_cvref_t<decltype(call::event_type(std::declval<T &&>()))>;
-    return subset_t::can_be_event(ui_event_identity<tEvt>{});
+    return subset_t::can_be_event(input_event_identity<tEvt>{});
   } else {
     return false;
   }
 }
-template <ui_events tEvt, typename T> constexpr bool is_event(T &&evt) {
+template <input_events tEvt, typename T> constexpr bool is_event(T &&evt) {
   if constexpr (can_be_event<tEvt, T>()) {
-    return static_cast<ui_events>(call::event_type(evt)) == tEvt;
+    return static_cast<input_events>(call::event_type(evt)) == tEvt;
   } else {
     unused(evt);
     return false;
   }
 }
 
-template <typename T, ui_events... tEvents>
+template <typename T, input_events... tEvents>
 concept event_types = (can_be_event<tEvents, T>() || ...);
 
-template <ui_events> struct default_event;
+template <input_events> struct default_event;
 
-template <ui_events tEvt>
-constexpr subset_ui_events<tEvt> event_type(default_event<tEvt> const &) {
+template <input_events tEvt>
+constexpr subset_input_events<tEvt> event_type(default_event<tEvt> const &) {
   return {};
 }
 
-template <> struct default_event<ui_events::system> {};
-template <> struct default_event<ui_events::mouse_exit> {};
-template <> struct default_event<ui_events::mouse_move> {
+template <> struct default_event<input_events::system> {};
+template <> struct default_event<input_events::mouse_exit> {};
+template <> struct default_event<input_events::mouse_move> {
   default_point_coordinate pos;
 };
-template <> struct default_event<ui_events::mouse_button_down> {
-  default_point_coordinate pos;
-  mouse_buttons button_id;
-};
-template <> struct default_event<ui_events::mouse_button_up> {
+template <> struct default_event<input_events::mouse_button_down> {
   default_point_coordinate pos;
   mouse_buttons button_id;
 };
-template <> struct default_event<ui_events::window_resized> {
+template <> struct default_event<input_events::mouse_button_up> {
+  default_point_coordinate pos;
+  mouse_buttons button_id;
+};
+template <> struct default_event<input_events::window_resized> {
   default_point_size_wh sz;
 };
 
 template <typename> constexpr bool is_cgui_default_event_v = false;
-template <ui_events tEvt>
+template <input_events tEvt>
 constexpr bool is_cgui_default_event_v<default_event<tEvt>> = true;
 
 template <typename T>
@@ -197,19 +198,19 @@ constexpr auto size_of(T const &t) {
   return t.sz;
 }
 
-using default_mouse_move_event = default_event<ui_events::mouse_move>;
-using default_mouse_down_event = default_event<ui_events::mouse_button_down>;
-using default_mouse_up_event = default_event<ui_events::mouse_button_up>;
-using default_mouse_exit_event = default_event<ui_events::mouse_exit>;
-using default_window_resized_event = default_event<ui_events::window_resized>;
+using default_mouse_move_event = default_event<input_events::mouse_move>;
+using default_mouse_down_event = default_event<input_events::mouse_button_down>;
+using default_mouse_up_event = default_event<input_events::mouse_button_up>;
+using default_mouse_exit_event = default_event<input_events::mouse_exit>;
+using default_window_resized_event = default_event<input_events::window_resized>;
 
 struct cgui_mouse_exit_event {
-  static constexpr subset_ui_events<ui_events::mouse_exit> event_type(auto &&) {
+  static constexpr subset_input_events<input_events::mouse_exit> event_type(auto &&) {
     return {};
   }
 };
 
-template <ui_events evt_val, typename F>
+template <input_events evt_val, typename F>
 class event_case_t : bp::empty_structs_optimiser<F> {
   template <typename Evt, typename... Ts>
   static constexpr bool valid_function =
@@ -243,20 +244,20 @@ public:
     return false;
   }
 };
-template <ui_events evt_v, typename T>
+template <input_events evt_v, typename T>
 constexpr event_case_t<evt_v, std::remove_cvref_t<T>> event_case(T &&in) {
   return event_case_t<evt_v, std::remove_cvref_t<T>>{std::forward<T>(in)};
 }
 
 namespace impl {
 template <typename> constexpr bool is_event_case_raw = false;
-template <ui_events e, typename T>
+template <input_events e, typename T>
 constexpr bool is_event_case_raw<event_case_t<e, T>> = true;
 template <typename T>
 constexpr bool is_event_case = is_event_case_raw<std::remove_cvref_t<T>>;
 } // namespace impl
 
-template <has_event_type Evt, typename Data, ui_events... evt_vs,
+template <has_event_type Evt, typename Data, input_events... evt_vs,
           typename... Fs>
   requires(bp::is_unique(evt_vs...) && !impl::is_event_case<Data> &&
            ((std::invocable<Fs> || std::invocable<Fs, Evt &&> ||
@@ -268,7 +269,7 @@ constexpr bool ui_event_switch(Evt &&e, Data &&d,
   // and d untouched if the case does not correspond to the correct event.
   return (cases(std::forward<Evt>(e), std::forward<Data>(d)) || ...);
 }
-template <has_event_type Evt, ui_events... evt_vs,
+template <has_event_type Evt, input_events... evt_vs,
           bp::invocable_or_invocable_args<Evt>... Fs>
   requires(bp::is_unique(evt_vs...))
 constexpr bool ui_event_switch(Evt &&e, event_case_t<evt_vs, Fs> &&...cases) {
@@ -348,7 +349,7 @@ public:
                 std::forward<decltype(args)>(args)...);
   }
 };
-template <typename Data, ui_events... evt_vs, typename... Fs>
+template <typename Data, input_events... evt_vs, typename... Fs>
 constexpr ui_event_switch_t<std::unwrap_ref_decay_t<Data>,
                             event_case_t<evt_vs, Fs>...>
 saved_ui_event_switch(Data &&d, event_case_t<evt_vs, Fs> &&...cases) {
---
+++ tests/cgui-events.cpp
@@ -0,0 +1,53 @@
+
+#include <cgui/cgui.hpp>
+
+#include <gmock/gmock.h>
+
+#include <cgui_test_utils.hpp>
+
+namespace cgui::tests {
+using namespace ::testing;
+
+TEST(UiEventsMatcher, Basics) // NOLINT
+{
+  auto f = MockFunction<void(std::string_view)>();
+  InSequence s;
+  EXPECT_CALL(f, Call(StrEq("Move")));
+  EXPECT_CALL(f, Call(StrEq("Down")));
+  EXPECT_CALL(f, Call(StrEq("Up")));
+
+  using enum input_events;
+  auto my_switch = [&f](auto &&evt) {
+    ui_event_switch(evt,
+                    event_case<mouse_button_down>([&f] { f.Call("Down"); }),
+                    event_case<mouse_button_up>([&f] { f.Call("Up"); }),
+                    event_case<mouse_move>([&f] { f.Call("Move"); }));
+  };
+  my_switch(default_mouse_move_event{});
+  my_switch(default_mouse_down_event{});
+  my_switch(default_mouse_up_event{});
+}
+
+TEST(UiEventsMatcher, BasicsState) // NOLINT
+{
+  auto f = MockFunction<void(std::string_view)>();
+  InSequence s;
+  EXPECT_CALL(f, Call(StrEq("Move")));
+  EXPECT_CALL(f, Call(StrEq("Down")));
+  EXPECT_CALL(f, Call(StrEq("Up")));
+
+  using enum input_events;
+  auto my_switch = [&f](auto &&evt) {
+    ui_event_switch(
+        evt, f, event_case<mouse_button_down>([](auto &&, auto &f2) {
+          f2.Call("Down");
+        }),
+        event_case<mouse_button_up>([](auto &&, auto &f2) { f2.Call("Up"); }),
+        event_case<mouse_move>([&f] { f.Call("Move"); }));
+  };
+  my_switch(default_mouse_move_event{});
+  my_switch(default_mouse_down_event{});
+  my_switch(default_mouse_up_event{});
+}
+
+}
